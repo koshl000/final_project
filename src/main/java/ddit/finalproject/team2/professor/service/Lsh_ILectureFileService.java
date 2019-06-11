8c51248c485e3eee7a8884bcec73214a0a0df459
jannlee31 <jannlee31@gmail.com>
2019-06-11 09:04:49 +0900

    이승한-채팅 수정

---
+++ src/main/java/ddit/finalproject/team2/professor/service/Lsh_ILectureFileService.java
@@ -0,0 +1,8 @@
+package ddit.finalproject.team2.professor.service;
+
+
+import ddit.finalproject.team2.vo.VideoFileVo;
+
+public interface Lsh_ILectureFileService {
+    void insertVideoFile(VideoFileVo v);
+}
