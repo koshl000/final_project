2b9dc4773eb06cd0e27ff3db2a0a5cb25478d860
jonggling123 <jonggling123@nate.com>
2019-06-12 00:28:26 +0900

    이종선 m2m 커밋

--- src/main/java/ddit/finalproject/team2/util/enumpack/RankType.java
+++ src/main/java/ddit/finalproject/team2/util/enumpack/RankType.java
@@ -3,20 +3,30 @@ package ddit.finalproject.team2.util.enumpack;
 import ddit.finalproject.team2.vo.GradeRankVo;
 
 public enum RankType {
-	A,B,C,D,F;
+	A("A"),B("B"),C("C"),D("D"),F("F");
+	
+	private String text;
+	
+	public String getText(){
+		return text;
+	}
+	
+	private RankType(String text){
+		this.text = text;
+	}
 	
 	public static int matchType(GradeRankVo rank){
 		int type = 4;
-		if(A.equals(rank.getGraderank_rank())){
+		if(A.getText().equals(rank.getGraderank_rank())){
 			type = 0;
 		}
-		if(B.equals(rank.getGraderank_rank())){
+		if(B.getText().equals(rank.getGraderank_rank())){
 			type = 1;
 		}
-		if(C.equals(rank.getGraderank_rank())){
+		if(C.getText().equals(rank.getGraderank_rank())){
 			type = 2;
 		}
-		if(D.equals(rank.getGraderank_rank())){
+		if(D.getText().equals(rank.getGraderank_rank())){
 			type = 3;
 		}
 		
